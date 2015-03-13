649bb40bd7a4adef5bb42d02339be65cc56e988c
George Mermigkas <yme@entersoft.gr>
2015-03-13 13:35:18 +0200

    added ngroute over jspm

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,21 @@
-let state = "compiled and loaded";
-console.log('compiled and loaded!');
+import angular from 'angular';
+import angularRoute from 'angular-route';
 
-export default {};
+console.log(angular.version);
+
+angular.module('app.controllers', []);
+angular.module('app.directives', []);
+angular.module('app.services', []);
+
+angular.module('app', [
+    'ngRoute',
+    'app.controllers',
+    'app.directives',
+    'app.services'
+]).
+config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/home.html',
+        });
+}]);
