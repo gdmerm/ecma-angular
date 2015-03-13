760001f3655ce292e45497afacdaf6aaf6de1855
George Mermigkas <yme@entersoft.gr>
2015-03-13 15:18:07 +0200

    added some missing dependencies

--- app/config.js
+++ app/config.js
@@ -13,11 +13,10 @@ System.config({
     "angular-route": "bower:angular-route@1.3.14",
     "angular-sanitize": "bower:angular-sanitize@1.3.14",
     "eswebapiangularjs": "bower:eswebapiangularjs@0.0.44",
-    "ngstorage": "bower:ngstorage@0.3.0",
-    "underscore": "npm:underscore@1.8.2",
     "log4javascript": "bower:log4javascript@1.4.9",
+    "ngstorage": "bower:ngstorage@0.3.0",
     "stacktrace-js": "bower:stacktrace-js@0.6.4",
-
+    "underscore": "npm:underscore@1.8.2",
     "bower:angular-route@1.3.14": {
       "angular": "bower:angular@1.3.14"
     },
@@ -30,7 +29,7 @@ System.config({
       "log4javascript": "bower:log4javascript@1.4.9",
       "ngstorage": "bower:ngstorage@0.3.0",
       "stacktrace-js": "bower:stacktrace-js@0.6.4",
-      "underscore": "npm:underscore@1.7.0"
+      "underscore": "bower:underscore@1.7.0"
     },
     "bower:ngstorage@0.3.0": {
       "angular": "bower:angular@1.3.14"
